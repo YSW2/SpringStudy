84d92889f86d02a2aeadade41438862e29bcb2c9
YSW2 <jrgyber@gmail.com>
2024-03-10 18:59:42 +0900

    MVC example commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -9,5 +10,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("asdf") String asdf, Model model){
+        model.addAttribute("name", asdf);
+        return "hello-template";
     }
 }
