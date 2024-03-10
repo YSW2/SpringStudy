33c473f11cc451afef9bf9553396d43ab5430674
YSW2 <jrgyber@gmail.com>
2024-03-10 19:13:35 +0900

    API example commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,5 +17,29 @@ public class HelloController {
     public String helloMvc(@RequestParam("asdf") String asdf, Model model){
         model.addAttribute("name", asdf);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("asdf") String asdf){
+        return "hello" + asdf;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello HelloApi(@RequestParam("asdf") String asdf){
+        Hello hello = new Hello();
+        hello.setName(asdf);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
